fea35ac8696c8468a2766c53cc2eef786ab7188f
Somnath Manna <somnath.manna@Somnaths-MacBook-Air.local>
2026-04-12 22:05:10 +0530

    Fix NAT gw allocations and '}'

--- terraform/modules/vpc/datasources-locals.tf
+++ terraform/modules/vpc/datasources-locals.tf
@@ -6,5 +6,5 @@ locals {
   azs = slice(data.aws_availability_zones.available.names, 0, var.num_azs)
   public_subnet_cidrs = { for k, az in local.azs : az => cidrsubnet(var.vpc_cidr, var.subnet_newbits, k)}
   private_subnet_cidrs = { for k, az in local.azs : az => cidrsubnet(var.vpc_cidr, var.subnet_newbits, k + 10)}
-  nat_subnet_map = var.single_nat_gateway ? { for k, az in local.azs : az => aws_subnet.public[az].id } : { for k, az in local.azs : az => aws_subnet.public[az].id }
+  nat_subnet_map = var.single_nat_gateway ? { for az in [local.azs[0]] : az => local.public_subnet_cidrs[az] } : local.public_subnet_cidrs
 }
